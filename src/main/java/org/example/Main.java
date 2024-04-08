bb2b2bd86190cea2ee323407f6bf66ca1e68f53c
ericdeback <1loveGithub>
2024-04-08 13:47:30 +0100

    various small code refactorings

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,10 @@ package org.example;
 import com.microsoft.playwright.*;
 
 import java.io.*;
+import java.util.regex.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.*;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -10,11 +14,19 @@ public class Main {
             BrowserType browserType = playwright.chromium();
             Browser browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
             BrowserContext context = browser.newContext();
+
             Page page = context.newPage();
             page.navigate("https://playwright.dev");
+
+            // Expect a title "to contain" a substring.
+            assertThat(page).hasTitle(Pattern.compile("Playwright"));
             System.out.println(page.title());
 
-            System.in.read();
+            page.locator("//html/body/div/div[2]/header/div/div/a").click();
+            assertThat(page).hasTitle("Installation | Playwright");
+
+            System.in.read(); //
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
